2ec10ac6132275aaad4e1ac4045749dd6d359199
Manoel <manoel.inf1.ep@gmail.com>
2019-10-25 20:24:17 -0200

    Adicionando +1 nas saídas para dar a impressão de que o array começa no 1

--- ALG_APS_Q1.cpp
+++ ALG_APS_Q1.cpp
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /* Grupo
-* Nome: Manoel Ribeiro 
-* Matricula: 2019101035
+* Nome: Manoel Ribeiro , Raiza Albuquerque, Eduardo Mello
+* Matricula: 2019101035, 2017101805, 2019100620
 */
 
 using namespace std;
@@ -15,7 +15,7 @@ int main() {
 
     for(int l = 0; l < 3; l++){
         for(int c = 0; c < 4; c++){
-            cout<<"Cordenada["<<l<<"]["<<c<<"] - Digite um numero : "; cin>>matriz[l][c];
+            cout<<"Cordenada["<<l+1<<"]["<<c+1<<"] - Digite um numero : "; cin>>matriz[l][c];
         }
     }
 
@@ -47,13 +47,13 @@ int main() {
 
     cout<<"----------------------"<<endl;
     cout<<"Maior: "<<maior<<endl;
-    cout<<"Linha do Maior: "<<lmaior<<endl;
-    cout<<"Coluna do Maior: "<<cmaior<<endl;
+    cout<<"Linha do Maior: "<<lmaior+1<<endl;
+    cout<<"Coluna do Maior: "<<cmaior+1<<endl;
     cout<<"Numero de Troca: "<<ntroca_maior<<endl;
     cout<<"----------------------"<<endl;
     cout<<"Menor: "<<menor<<endl;
-    cout<<"Linha do Menor: "<<lmenor<<endl;
-    cout<<"Coluna do Menor: "<<cmenor<<endl;
+    cout<<"Linha do Menor: "<<lmenor+1<<endl;
+    cout<<"Coluna do Menor: "<<cmenor+1<<endl;
     cout<<"Numero de Troca: "<<ntroca_menor<<endl;
     
     return 0;
