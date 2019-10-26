0651432747c2165a3939552dea39f0eebadb74f2
Manoel <manoel.inf1.ep@gmail.com>
2019-10-26 18:13:45 -0200

    Adicionando novas funcionalidades (carregar, salvar, pesquisar e exibir)

--- aps_comp_prog/APS_COMP_PROG.cpp
+++ aps_comp_prog/APS_COMP_PROG.cpp
@@ -5,84 +5,198 @@
 
 using namespace std;
 
-char nome[50], sexo[15];
-int idade, matricula;
-float nota1, nota2, nota3, media;
+char nome[50][50], sexo[15][50], nome_arq[50], nome_arq_carregado[50];
+int idade[50], matricula[50], contadorAlunos = 0;
+float nota1[50], nota2[50], nota3[50], media[50];
 
-char nome_arq[50];
 FILE *arq;
 
+void exibeAlunoPorIndice(int i){
+	cout<<"Matricula: "<<matricula[i]<<endl;
+	cout<<"Nome: "<<nome[i]<<endl;
+	cout<<"Idade: "<<idade[i]<<endl;
+	cout<<"Sexo: "<<sexo[i]<<endl;
+	cout<<"AV1: "<<nota1[i]<<endl;
+	cout<<"AV2: "<<nota2[i]<<endl;
+	cout<<"AV3: "<<nota3[i]<<endl;
+	cout<<"Media: "<<media[i]<<endl;
+}
+
 int calcMediaUni(int a, int b, int c){
-  	int n1, n2;
-	 if(a > b && c < b){
-	 	n1 = a; n2 = b;
-	 }else if(b > a && a < c){
-	 	n1 = b; n2 = c;
-	 }else if(c > a && b < c){
-	 	n1 = c; n2 = b; 
-	 }else if(c > b && a < c){
-	 	n1 = c; n2 = a;
-	 }
-	return (n1 + n2)/2; 
+	return (a + b + c)/3; 
+}
+
+void retornaAlunosPorFaixaDeNotaMaior(int nota){
+	bool encontrado = false; 
+	system("cls");
+	for(int i = 0; i < contadorAlunos; i++){
+		if(media[i] > nota){
+			encontrado = true;
+			cout<<"----------------------------"<<endl;
+			exibeAlunoPorIndice(i);
+			cout<<"----------------------------"<<endl;
+		}
+	}
+	if(!encontrado)
+		cout<<"NENHUM ALUNO FOI ENCONTRADO."<<endl;
+}
+
+void retornaAlunosPorFaixaDeNotaMenor(int nota){
+	bool encontrado = false; 
+	system("cls");
+	for(int i = 0; i < contadorAlunos; i++){
+		if(media[i] < nota){
+			encontrado = true;
+			cout<<"----------------------------"<<endl;
+			exibeAlunoPorIndice(i);
+			cout<<"----------------------------"<<endl;
+		}
+	}
+	if(!encontrado)
+		cout<<"NENHUM ALUNO FOI ENCONTRADO."<<endl;
+}
+
+bool verificaPermissaoParaAddAluno(){
+	return (contadorAlunos == 50) ? false : true;
+}
+
+bool verificaAlunoExistente(int mat){
+	for(int i = 0; i < contadorAlunos; i++){
+		if(matricula[i] == mat){
+			return true;
+		}		
+	}
+	return false;
 }
 
 void addAluno(){
 	system("cls");
 	cout<<"UNICARIOCA - BANCO DE DADOS"<<endl;
 	cout<<"ADICIONAR USUARIO"<<endl;
-	cout<<"Matricula: "; cin>>matricula;
-	cout<<"Nome: "; cin>>nome;
-	cout<<"Idade: "; cin>>idade;
-	cout<<"Sexo: "; cin>>sexo;
-	cout<<"Nota 1: "; cin>>nota1;
-	cout<<"Nota 2: "; cin>>nota2;
-	cout<<"Nota 3: "; cin>>nota3;
-	media = calcMediaUni(nota1, nota2, nota3);
-	
-	cout<<endl<<"Nome do arquivo: "; cin>>nome_arq;
-	strcat(nome_arq,".txt");
-	arq = fopen(nome_arq,"w");
-	if (arq == NULL)
-		cout<<"ERRO NO ARQUIVO"<<endl;
-	else{
-		fprintf(arq,"Matricula = %d\n", &matricula);
-		fprintf(arq,"Nome = %s\n", nome);
-		fprintf(arq,"Idade = %d\n", &idade);
-		fprintf(arq,"Sexo = %s\n", sexo);
-		fprintf(arq,"Nota 1 = %0.1f\n", nota1);
-		fprintf(arq,"Nota 2 = %0.1f\n", nota2);
-		fprintf(arq,"Nota 3 = %0.1f\n", nota3);
-		fprintf(arq,"Media = %0.1f\n", media);	
+	if(verificaPermissaoParaAddAluno()){
+		cout<<"Matricula: "; cin>>matricula[contadorAlunos];
+		if(verificaAlunoExistente(matricula[contadorAlunos])){
+		cout<<"ALUNO JA CADASTRADO"<<endl;	
+		}else{
+			cout<<"Nome: "; cin>>nome[contadorAlunos];
+			cout<<"Idade: "; cin>>idade[contadorAlunos];
+			cout<<"Sexo (M/F): "; cin>>sexo[contadorAlunos];
+			cout<<"Nota 1: "; cin>>nota1[contadorAlunos];
+			cout<<"Nota 2: "; cin>>nota2[contadorAlunos];
+			cout<<"Nota 3: "; cin>>nota3[contadorAlunos];
+			media[contadorAlunos] = calcMediaUni(nota1[contadorAlunos], 
+												 nota2[contadorAlunos], 
+												 nota3[contadorAlunos]);
+			contadorAlunos++;
+		}
+	}else{
+		cout<<"NNUMERO LIMITE DE CADASTROS ALCANCADO."<<endl;
 	}
-	fclose(arq);
 	system("pause");
 }
 
-void exibirAluno(){
-	/*system("cls");
-	cout<<"UNICARIOCA - BANCO DE DADOS"<<endl;
-	cout<<"EXIBIR USUARIO"<<endl;
-	cout<<"Nome do arquivo: "; cin>>nome_arq;
-	strcat(nome_arq,".txt");
-	arq = fopen(nome_arq,"r");
-	if (arq == NULL)
-		cout<<"ARQUIVO NAO EXISTE"<<endl;
-	else{
-		fseek(arq,6,0);
-		fscanf(arq,"%d\n",&matricula);
-		fseek(arq,7,1);
-		fscanf(arq,"%s\n",nome);
-		fseek(arq,8,1);
-		fscanf(arq,"%d\n",&idade);
-		fseek(arq,9,1);
-		fscanf(arq,"%s\n",sexo);
-		cout<<"CPF: "<<cpf<<endl;
-		cout<<"Nome: "<<nome<<endl;
-		cout<<"Idade: "<<idade<<endl;
-		cout<<"E-mail: "<<email<<endl;
+void persisteAlunosNoArquivoExterno(){
+	for(int i = 0; i < contadorAlunos; i++){
+		fprintf(arq,"Matricula = %d\n", matricula[i]);
+		fprintf(arq,"Nome = %s\n", nome[i]);
+		fprintf(arq,"Idade = %d\n", idade[i]);
+		fprintf(arq,"Sexo = %s\n", sexo[i]);
+		fprintf(arq,"AV1 = %0.1f\n", nota1[i]);
+		fprintf(arq,"AV2 = %0.1f\n", nota2[i]);
+		fprintf(arq,"AV3 = %0.1f\n", nota3[i]);
+		fprintf(arq,"Media = %0.1f\n", media[i]);
+		fprintf(arq,"-------\n");
+
+		if(i+1 == contadorAlunos){
+			fprintf(arq,"Total = %d", i+1);
+		}
 	}
-	fclose(arq);
-	system("pause");*/
+}
+
+void salvarAlunos(){
+	system("cls");
+	if(contadorAlunos != 0){
+		cout<<endl<<"NOME DO ARQUIVO: "; cin>>nome_arq;
+		strcat(nome_arq,".txt");
+		arq = fopen(nome_arq,"w");
+		if (arq == NULL)
+			cout<<"ERRO NO ARQUIVO"<<endl;
+		else{
+			persisteAlunosNoArquivoExterno();
+			cout<<"ALUNOS SALVOS NO ARQUIVO: "<<nome_arq<<endl;
+		}
+		fclose(arq);
+	}else{
+		cout<<"NENHUM ALUNO ENCONTRADO."<<endl;
+	}
+
+	system("pause");
+}
+
+void exibeAprovados(){
+	int opc;
+	system("cls");
+	cout<<"UNICARIOCA - BANCO DE DADOS"<<endl;
+	cout<<"USUARIOS APROVADOS"<<endl;
+	cout<<"[1] MEDIA >=7"<<endl;
+	cout<<"[2] MEDIA >=9"<<endl;
+	cout<<"[0] VOLTAR"<<endl;
+	cout<<"OPCAO: "; cin>>opc;
+	switch (opc){
+		case 1: retornaAlunosPorFaixaDeNotaMaior(6); break;
+		case 2: retornaAlunosPorFaixaDeNotaMaior(8); break;
+	}
+}
+
+void exibeReprovados(){
+	int opc;
+	system("cls");
+	cout<<"UNICARIOCA - BANCO DE DADOS"<<endl;
+	cout<<"USUARIOS REPROVADOS"<<endl;
+	cout<<"[1] MEDIA < 7"<<endl;
+	cout<<"[2] MEDIA < 5"<<endl;
+	cout<<"[0] VOLTAR"<<endl;
+	cout<<"OPCAO: "; cin>>opc;
+	switch (opc){
+		case 1: retornaAlunosPorFaixaDeNotaMenor(7); break;
+		case 2: retornaAlunosPorFaixaDeNotaMenor(5); break;
+	}
+}
+
+void exibeTodosAlunos(){
+	system("cls");
+	for(int i = 0; i < contadorAlunos; i++){
+		cout<<"----------------------------"<<endl;
+		exibeAlunoPorIndice(i);
+		cout<<"----------------------------"<<endl;
+	}
+}
+
+void exibirAluno(){
+	int opc;
+	system("cls");
+	cout<<"UNICARIOCA - BANCO DE DADOS"<<endl;
+	cout<<"EXIBIR USUARIOS"<<endl;
+	cout<<"[1] APROVADOS"<<endl;
+	cout<<"[2] REPROVADOS"<<endl;
+	cout<<"[3] TODOS"<<endl;
+	cout<<"[0] VOLTAR"<<endl;
+	cout<<"OPCAO: "; cin>>opc;
+	switch (opc){
+		case 1: exibeAprovados(); break;
+		case 2: exibeReprovados(); break;
+		case 3: exibeTodosAlunos(); break;
+	}
+	system("pause");
+}
+
+int retornaPosicaoDoAlunoPorMatricula(int mat_pesq){
+	for(int i = 0; i < contadorAlunos; i++){
+		if(matricula[i] == mat_pesq){
+			return i;
+		}
+	}
+	return 51;
 }
 
 void pesquisarAluno(){
@@ -90,51 +204,75 @@ void pesquisarAluno(){
 	system("cls");
 	cout<<"UNICARIOCA - BANCO DE DADOS"<<endl;
 	cout<<"BUSCAR USUARIO"<<endl;
-	cout<<"Matricula para pesquisa: "; cin>>mat_pesquisa;
-	
-	cout<<"Nome do arquivo: "; cin>>nome_arq;
+	cout<<"MATRICULA PARA PESQUISA: "; cin>>mat_pesquisa;
+	int i = retornaPosicaoDoAlunoPorMatricula(mat_pesquisa);
+	system("cls");
+	if(i != 51){
+		exibeAlunoPorIndice(i);
+	}else{
+		cout<<"ALUNO NAO ENCONTRADO."<<endl;
+	}
+	system("pause");
+}
+
+void carregarAlunosArqExterno(){
+	system("cls");
+	int total;
+	cout<<"NOME DO ARQUIVO: "; cin>>nome_arq;
 	strcat(nome_arq,".txt");
 	arq = fopen(nome_arq,"r");
-	if (arq == NULL)
+	if (arq == NULL){
 		cout<<"ARQUIVO NAO EXISTE"<<endl;
-	else{
-		fseek(arq,12,0);
-		fscanf(arq,"%d\n", &matricula);
-		fseek(arq,7,1);
-		fscanf(arq,"%s\n", nome);
-		fseek(arq,8,1);
-		fscanf(arq,"%d\n", &idade);
-		fseek(arq,7,1);
-		fscanf(arq,"%s\n", sexo);
-		fseek(arq,9,1);
-		fscanf(arq,"%f\n", nota1);
-		fseek(arq,9,1);
-		fscanf(arq,"%f\n", nota2);
-		fseek(arq,9,1);
-		fscanf(arq,"%f\n", nota3);
-		fseek(arq,8,1);
-		fscanf(arq,"%f\n", media);
-		if (mat_pesquisa == matricula){
-			cout<<"Matricula: "<<matricula<<endl;
-			cout<<"Nome: "<<nome<<endl;
-			cout<<"Idade: "<<idade<<endl;
-			cout<<"Sexo: "<<sexo<<endl;
-			cout<<"Nota 1: "<<nota1<<endl;
-			cout<<"Nota 2: "<<nota2<<endl;
-			cout<<"Nota 3: "<<nota3<<endl;
-			cout<<"Media: "<<media<<endl;
-		} else{
-			cout<<"MATRICULA NAO ENCONTRADO"<<endl;
+	} else{
+		fseek(arq, -1,2);
+		fscanf(arq,"%d", &total);
+		fseek(arq, 0,0);
+
+		for(int i = contadorAlunos; i < total; i++){
+			fseek(arq, 12,1);
+			fscanf(arq,"%d \n", &matricula[i]);
+			fseek(arq, 7,1);
+			fscanf(arq,"%s \n", nome[i]);
+			fseek(arq, 8,1);
+			fscanf(arq,"%d \n", &idade[i]);
+			fseek(arq, 7,1);
+			fscanf(arq,"%s \n", sexo[i]);
+			fseek(arq, 6,1);
+			fscanf(arq,"%f \n", &nota1[i]);
+			fseek(arq, 6,1);
+			fscanf(arq,"%f \n", &nota2[i]);
+			fseek(arq, 6,1);
+			fscanf(arq,"%f \n", &nota3[i]);
+			fseek(arq, 8,1);
+			fscanf(arq,"%f \n", &media[i]);
+			fseek(arq, 8,1);
+			contadorAlunos++;
 		}
+		cout<<"ALUNOS CARREGADOS COM SUCESSO."<<endl;
 	}
-		fclose(arq);
-		system("pause");
+	fclose(arq);
+	system("pause");
+}
+
+void exibeSalvarCarregar(){
+	system("cls");
+	int opc;
+	cout<<"UNICARIOCA - BANCO DE DADOS"<<endl;
+	cout<<"SALVAR/CARREGAR ARQUIVO"<<endl;
+	cout<<"[1] SALVAR"<<endl;
+	cout<<"[2] CARREGAR"<<endl;
+	cout<<"[0] VOLTAR"<<endl;
+	cout<<"OPCAO: "; cin>>opc;
+	switch (opc){
+		case 1: salvarAlunos(); break;
+		case 2: carregarAlunosArqExterno(); break;
+	}
 }
 
 main(){
 	int op;
 	do{
-		system("cls");
+		system("cls");	
 		cout<<"UNICARIOCA - BANCO DE DADOS"<<endl;
 		cout<<"[1] ADICIONAR"<<endl;
 		cout<<"[2] PESQUISAR"<<endl;
@@ -146,6 +284,7 @@ main(){
 			case 1: addAluno(); break;
 			case 2: pesquisarAluno(); break;
 			case 3: exibirAluno(); break;
+			case 4: exibeSalvarCarregar(); break;
 		}
 	}while(op!=0);
 }
