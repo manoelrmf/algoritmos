b5ae94c66c27273f575657caec8830ce5f6d6d0f
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-02-22 19:43:25 -0300

    Estrutura inicial

---
+++ estrutura_de_dados/EstruturaInicial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <malloc.h>
+#define alturaMaxima 225
+
+typedef struct 
+{
+    int peso;
+    int altura;
+} PesoAltura;
+
+int main (){
+   PesoAltura* pessoa1 = (PesoAltura*) malloc(sizeof(PesoAltura));
+   pessoa1->peso = 80;
+   pessoa1->altura = 185;
+   printf("Peso: %i, Altura %i. \n", pessoa1->peso, pessoa1->altura);
+   if(pessoa1->altura > alturaMaxima) printf("Altura acima da maxima \n");
+   else printf("Altura abaixo da maxima \n");
+    return 0;
+}
