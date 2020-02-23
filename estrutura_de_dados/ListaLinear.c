9128d95a392dbfbeba2faf58126e576b2949f97f
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-02-23 13:01:52 -0300

    Update ListaLinear.c

--- estrutura_de_dados/ListaLinear.c
+++ estrutura_de_dados/ListaLinear.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <malloc.h>
+#include <stdbool.h>
+
 #define MAX 50
 
 typedef int TIPOCHAVE;
 
 typedef struct 
 {
-    TIPOCHAVE chave;
+    TIPOCHAVE chave;    
 } REGISTRO;
 
 typedef struct 
@@ -54,6 +56,34 @@ bool inserirElemento(LISTA* l, REGISTRO reg, int i){
     return true;
 }
 
+bool excluirElemento(TIPOCHAVE ch, LISTA* l){
+    int pos, j;
+    pos = buscaSequencial(l,ch);
+    if(pos == -1) return false;
+    for(j = pos; j < l->nroElem - 1; j++){
+        l->A[j] = l->A[j+1];
+    }
+    l->nroElem--;
+    return true;
+}
+
+void reinicializarLista(LISTA* l){
+    l->nroElem = 0;
+}
+
 int main (){
-    
+    LISTA lista;
+    inicializa(&lista);
+    REGISTRO reg;
+    reg.chave = 0;
+    if(inserirElemento(&lista, reg, 0)){
+        printf("Inserido com sucesso \n");
+    }
+
+    if(buscaSequencial(&lista, 1) != -1){
+        printf("Encontrado \n");
+    } else{
+        printf ("Nao encontrado \n");
+    }
+
 }
