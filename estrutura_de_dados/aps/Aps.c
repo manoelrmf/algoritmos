59991e5ebe3f2e4aa09d211ebe31db834eaca855
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-03-21 20:18:00 -0300

    Update Aps.c

--- estrutura_de_dados/aps/Aps.c
+++ estrutura_de_dados/aps/Aps.c
@@ -6,45 +6,70 @@
 
 typedef int CHAVE;
 
-typedef struct{
+typedef struct
+{
     CHAVE chave;
 } REGISTRO;
 
-typedef struct{
+typedef struct
+{
     REGISTRO A[MAX];
     int numElementos;
 } LISTA;
 
-LISTA lista;
+typedef struct
+{
+    int topo;
+    REGISTRO A[MAX];
+} PILHA;
 
-int buscaSequencial(LISTA* listaAbuscar, CHAVE ch){
+LISTA lista;
+PILHA pilha;
+
+
+void limpaConsole()
+{
+    system("clear");
+}
+
+// Funções referentes ao tipo de estrutura lista
+
+
+
+int buscaSequencial(LISTA *listaAbuscar, CHAVE ch)
+{
     int i = 0;
-    for(i = 0; i < listaAbuscar->numElementos; i++){
-        if(ch == listaAbuscar->A[i].chave) return i;
+    for (i = 0; i < listaAbuscar->numElementos; i++)
+    {
+        if (ch == listaAbuscar->A[i].chave)
+            return i;
     }
 
     return -1;
 }
 
-// Funções referentes ao tipo de estrutura lista
-
-bool excluirElemento(CHAVE ch, LISTA* listaAexcluir){
+bool excluirElemento(CHAVE ch, LISTA *listaAexcluir)
+{
     int pos, j;
-    pos = buscaSequencial(listaAexcluir,ch);
-    if(pos == -1) return false;
-    for(j = pos; j < listaAexcluir->numElementos - 1; j++){
-        listaAexcluir->A[j] = listaAexcluir->A[j+1];
+    pos = buscaSequencial(listaAexcluir, ch);
+    if (pos == -1)
+        return false;
+    for (j = pos; j < listaAexcluir->numElementos - 1; j++)
+    {
+        listaAexcluir->A[j] = listaAexcluir->A[j + 1];
     }
     listaAexcluir->numElementos--;
     return true;
 }
 
-bool inserirElemento(LISTA* l, REGISTRO reg, int i){
+bool inserirElemento(LISTA *l, REGISTRO reg, int i)
+{
     int j;
-    if((l->numElementos == MAX) || (i < 0) || (i > l->numElementos))
+    if ((l->numElementos == MAX) || (i < 0) || (i > l->numElementos))
         return false;
-    for(j = l->numElementos; j > i; j--){
-        l->A[j] = l->A[j-1];
+    for (j = l->numElementos; j > i; j--)
+    {
+        l->A[j] = l->A[j - 1];
     }
     l->A[i] = reg;
     l->numElementos++;
@@ -52,19 +77,23 @@ bool inserirElemento(LISTA* l, REGISTRO reg, int i){
     return true;
 }
 
-void exibir(LISTA* l){
+void exibir(LISTA *l)
+{
     int i;
-    printf("\n Lista: \" ");
-    for(i = 0; i < l->numElementos; i++){
+    printf("\n Lista --> \" ");
+    for (i = 0; i < l->numElementos; i++)
+    {
         printf("%i ", l->A[i].chave);
     }
     printf("\"\n");
 }
-void inicializa(LISTA* lista){
+void inicializa(LISTA *lista)
+{
     lista->numElementos = 0;
 }
 
-int escolhaOperacaoLista(){
+int escolhaOperacao()
+{
     int id;
     printf(" \n");
     printf(" 2 - Inserir elemento \n");
@@ -76,103 +105,226 @@ int escolhaOperacaoLista(){
 }
 
 void operacoesLista();
-void excluirElemntoDaLista(){
+void excluirElemntoDaLista(LISTA *listaAexcluirElemento)
+{
+    if (listaAexcluirElemento->numElementos == 0)
+    {
+        limpaConsole();
+        printf(" A lista está vazia! \n");
+        operacoesLista();
+        return;
+    }
     int numero;
-     printf(" Qual elemento deseja excluir? ");
-        scanf("%i", &numero);
-        if (excluirElemento(numero, &lista))
-        {
-            printf("\n Excluido com sucesso! \n");
-            operacoesLista();
-        }
-        else
-        {
-            printf("\n Erro ao excluir! \n");
-            operacoesLista();
-        }
+    printf(" Qual elemento deseja excluir? ");
+    scanf("%i", &numero);
+    if (excluirElemento(numero, &lista))
+    {
+        limpaConsole();
+        printf("\n Excluido com sucesso! \n");
+        operacoesLista();
+    }
+    else
+    {
+        limpaConsole();
+        printf("\n Valor não econtrado! \n");
+        operacoesLista();
+    }
 }
 
-void ordenaLista(LISTA* listaAOrdenar){
+void ordenaLista(LISTA *listaAOrdenar)
+{
     int contador, aux, i, tamanhoVet = listaAOrdenar->numElementos;
 
-     for (contador = 1; contador < tamanhoVet; contador++) {
-	   for (i = 0; i < tamanhoVet - 1; i++) {
-	     if (listaAOrdenar->A[i].chave > listaAOrdenar->A[i + 1].chave) {
-	       aux = listaAOrdenar->A[i].chave;
-	       listaAOrdenar->A[i].chave = listaAOrdenar->A[i + 1].chave;
-	       listaAOrdenar->A[i + 1].chave = aux;
-	    }
-	  }
-	}
+    for (contador = 1; contador < tamanhoVet; contador++)
+    {
+        for (i = 0; i < tamanhoVet - 1; i++)
+        {
+            if (listaAOrdenar->A[i].chave > listaAOrdenar->A[i + 1].chave)
+            {
+                aux = listaAOrdenar->A[i].chave;
+                listaAOrdenar->A[i].chave = listaAOrdenar->A[i + 1].chave;
+                listaAOrdenar->A[i + 1].chave = aux;
+            }
+        }
+    }
 }
 
-bool verificaVagaLista(LISTA* listaAverifica){
+bool verificaVagaLista(LISTA *listaAverifica)
+{
     return listaAverifica->numElementos < 10;
 }
 
 void operacoesLista();
 
-void inserirNaLista(){
+void inserirNaLista()
+{
     printf(" \n");
-    printf("\nTipo de estrutura selecionada: 3 - Lista \n");
     int numero;
     exibir(&lista);
     printf(" \n");
-    printf("Digite um número para incluir na lista: ");
+    printf(" Digite um número para incluir na lista: ");
     scanf("%i", &numero);
-    if(verificaVagaLista(&lista)){ 
+    if (verificaVagaLista(&lista))
+    {
         REGISTRO registro;
         registro.chave = numero;
-        if(inserirElemento(&lista, registro, 0)){
-        ordenaLista(&lista);
-        inserirNaLista();
+        if (inserirElemento(&lista, registro, 0))
+        {
+            ordenaLista(&lista);
+            limpaConsole();
+            operacoesLista();
         }
-    }else{
+    }
+    else
+    {
         printf(" \n");
         exibir(&lista);
-        printf("\nLista cheia! \n");
+        printf("\n Lista cheia! \n");
         printf("\n 1 - Excluir elemento ");
         printf("\n 0 - Voltar \n");
         printf(" \n");
         printf(" O que deseja fazer? ");
         scanf("%i", &numero);
-        if(numero == 1){
-           excluirElemntoDaLista();
-        }else{
+        if (numero == 1)
+        {
+            excluirElemntoDaLista(&lista);
+        }
+        else
+        {
+            limpaConsole();
             operacoesLista();
         }
-
     }
 }
 int main();
-void operacoesLista(){
-    printf("\n Tipo de estrutura selecionada: 3 - Lista \n");
+void operacoesLista()
+{
     exibir(&lista);
-    int tipoOperacao = escolhaOperacaoLista();
-    switch (tipoOperacao){
+    int tipoOperacao = escolhaOperacao();
+    switch (tipoOperacao)
+    {
     case 2:
+        limpaConsole();
         inserirNaLista();
         break;
     case 1:
-        excluirElemntoDaLista();
+        excluirElemntoDaLista(&lista);
+        break;
+    case 0:
+        limpaConsole();
+        main();
         break;
     default:
-        main();
+        limpaConsole();
+        printf(" Valor inválido! \n");
+        operacoesLista();
         break;
     }
 }
 
 // Funções referentes ao tipo de estrutura fila
-void fila(){
+void fila()
+{
     printf("Fila");
 }
 
 // Funções referentes ao tipo de estrutura pilha
-void pilha(){
-    printf("Pilha");    
+
+void inicializaPilha(PILHA *pilha) {
+   pilha->topo = 0;
 }
 
-int escolheEstrutura(){
+
+bool verificaPilhaVazia(PILHA *pilha){
+    return pilha->topo == 0 ? true : false;
+}
+
+bool verificaPilhaCheia(PILHA *pilha) {
+	int tamanho = sizeof(pilha->A)/sizeof(int);
+    return pilha->topo < tamanho  ? false : true ; 
+}
+
+void inserirPilha(PILHA *pilha, REGISTRO reg){
+    pilha->A[pilha->topo++] = reg;
+}
+
+void removePilha(PILHA *pilha){
+    pilha->A[--pilha->topo];
+}
+
+void exibirPilha(PILHA *p) {
+    printf("\n Pilha --> \" ");
+	for (int i = 0; i < p->topo; i++) {
+        printf("%i ", p->A[i].chave);
+	}
+    printf("\"\n");
+}
+void operacoesPilha();
+void inserirNaPilha()
+{
+    printf(" \n");
+    int numero;
+    exibirPilha(&pilha);
+    printf(" \n");
+    printf(" Digite um número para incluir na pilha: ");
+    scanf("%i", &numero);
+    if (!verificaPilhaCheia(&pilha))
+    {
+        REGISTRO registro;
+        registro.chave = numero;
+        inserirPilha(&pilha, registro);
+        limpaConsole();
+        operacoesPilha();
+    }
+    else
+    {
+        printf(" \n");
+        exibirPilha(&pilha);
+        printf("\n Pilha cheia! \n");
+        printf("\n 1 - Excluir elemento ");
+        printf("\n 0 - Voltar \n");
+        printf(" \n");
+        printf(" O que deseja fazer? ");
+        scanf("%i", &numero);
+        if (numero == 1)
+        {
+            excluirElemntoDaLista(&lista);
+        }
+        else
+        {
+            limpaConsole();
+            operacoesPilha();
+        }
+    }
+}
+
+void operacoesPilha()
+{
+    exibirPilha(&pilha);
+    int tipoOperacao = escolhaOperacao();
+    switch (tipoOperacao)
+    {
+    case 2:
+        limpaConsole();
+        inserirNaPilha();
+        break;
+    case 1:
+        printf("excluir");
+        break;
+    case 0:
+        limpaConsole();
+        main();
+        break;
+    default:
+        limpaConsole();
+        printf(" Valor inválido! \n");
+        operacoesPilha();
+        break;
+    }
+}
+
+int escolheEstrutura()
+{
     int id;
     printf(" \n");
     printf(" 3 - Lista \n");
@@ -184,18 +336,24 @@ int escolheEstrutura(){
     return id;
 }
 
-int main (){
-   int tipoEstrutura = escolheEstrutura();
-    switch (tipoEstrutura){
+int main()
+{
+    int tipoEstrutura = escolheEstrutura();
+    switch (tipoEstrutura)
+    {
     case 3:
         inicializa(&lista);
+        limpaConsole();
         operacoesLista();
         break;
     case 2:
+        limpaConsole();
         fila();
         break;
     case 1:
-        pilha();        
+        inicializaPilha(&pilha);
+        limpaConsole();
+        operacoesPilha();
         break;
     default:
         printf("\n Até a próxima! \n");
